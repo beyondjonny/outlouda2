5bd3d8288eef6efc7e6e58efe9866a1c5b9853be
zsh git_setup.sh <jonny.alexander.davis@gmail.com>
2021-01-06 22:18:05 +0000

    test

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,10 @@
 class Question < ApplicationRecord
   belongs_to :practice
+  def next
+    Question.where("id > ?", id).order(id: :asc).limit(1).first
+  end
+
+  def prev
+    Question.where("id < ?", id).order(id: :desc).limit(1).first
+  end
 end
