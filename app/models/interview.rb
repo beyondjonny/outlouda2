f999861e7ca575973952a3a71bf9cdadc812c976
zsh git_setup.sh <jonny.alexander.davis@gmail.com>
2021-02-14 12:09:52 +0000

    amit4545454

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -9,11 +9,4 @@ class Interview < ApplicationRecord
   def days_remaining
     -(Date.today - self.final_date.to_date).to_i if self.final_date
   end
-
-  include PgSearch::Model
-  pg_search_scope :search_by_name_and_date,
-    against: [:name, :open_date, :final_date, :company],
-    using: {
-      tsearch: { prefix: true }
-    }
 end
